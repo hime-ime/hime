hime-ime/hime

commit 38df8f3d2508e2df356f22602fcacc68b1aaa9fe

Remove resize grip from GTK2+ for Ubuntu, close #59

src/hime-gtk-compatible.h
@@ -69,8 +69,11 @@
 #define gdk_drawable_get_screen gdk_window_get_screen
 #endif
 
-#if !GTK_CHECK_VERSION(2,91,1)
+#ifndef gtk_window_set_has_resize_grip
 #define gtk_window_set_has_resize_grip(x,y);
+#endif
+
+#if !GTK_CHECK_VERSION(2,91,1)
 #define gtk_widget_set_hexpand(x,y);
 #define gtk_widget_set_vexpand(x,y);
 #endif

commit 053280464c5dedc38db8142ed967a82c16c140fc

Fix GTK+ 3.x regression caused by commit 38df8f3

src/hime-gtk-compatible.h
@@ -69,7 +69,7 @@
 #define gdk_drawable_get_screen gdk_window_get_screen
 #endif
 
-#ifndef gtk_window_set_has_resize_grip
+#if !defined(gtk_window_set_has_resize_grip) && !GTK_CHECK_VERSION(2,91,1)
 #define gtk_window_set_has_resize_grip(x,y);
 #endif
 