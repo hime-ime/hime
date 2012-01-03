5c356004b20fcdefffb45f8ae60df45216687aee
letoh <letoh.tw@gmail.com>
2012-01-04 00:34:01 +0800

    improve dbg function

--- src/util.h
+++ src/util.h
@@ -1,7 +1,12 @@
 void p_err(char *fmt,...);
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+
 #if DEBUG
 void __hime_dbg_(char *fmt,...);
-#define dbg(...) __hime_dbg_(__VA_ARGS__)
+#define dbg(fmt,...) __hime_dbg_("%s:%d: " fmt, __func__, __LINE__, ## __VA_ARGS__)
 #else
 #define dbg(...) do {} while (0)
 #endif
