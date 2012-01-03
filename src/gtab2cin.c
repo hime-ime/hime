e2c7fdedb332eac94770344736e7a8eed8428b1e
Solomon Huang <kaichanh@gmail.com>
2012-01-03 14:29:51 +0800

    Update copyright year.

--- src/gtab2cin.c
+++ src/gtab2cin.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2011 Huang, Kai-Chang (Solomon Huang) <kaichanh@gmail.com>
+/* Copyright (C) 2011-2012 Huang, Kai-Chang (Solomon Huang) <kaichanh@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
