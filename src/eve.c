8b8d8821dfef1f286f169240b407e1ba9237ffd7
Wen-Yen Chuang <caleb@calno.com>
2012-01-08 21:22:14 +0800

    Revert commit 1b7c228 due to segfaults hell

--- src/eve.c
+++ src/eve.c
@@ -534,7 +534,7 @@ void move_in_win(ClientState *cs, int x, int y)
         module_cb1(cs)->module_move_win(x, y);
       break;
     default:
-      if (cs->in_method < 0)
+      if (!cs->in_method)
         return;
       move_win_gtab(x, y);
   }
@@ -758,7 +758,7 @@ void init_state_chinese(ClientState *cs)
 {
   cs->im_state = HIME_STATE_CHINESE;
   set_tsin_pho_mode0(cs);
-  if (cs->in_method < 0)
+  if (!cs->in_method)
 #if UNIX
     init_in_method(default_input_method);
 #else
