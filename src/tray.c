98a0c602e9e6bfcf76a0e9e34be7539c9a6a94d2
Tommy Tsim <tytsim@gmail.com>
2012-02-26 00:12:21 +0800

    Fix commit acb741a319b6e889cb3a191db950abb0366d1fc0.

--- src/tray.c
+++ src/tray.c
@@ -153,10 +153,8 @@ void load_tray_icon()
     pixbuf = gdk_pixbuf_new_from_file_at_size(icon_fname, iw, ih, NULL);
   }
   char *iconame = HIME_TRAY_PNG;
-//  if (current_CS && current_CS->in_method && inmd)
-// Workaround due to issue #161 (caleb-/hime) :
-  if (current_CS && current_CS->im_state != HIME_STATE_DISABLED && current_CS->im_state != HIME_STATE_ENG_FULL)
-    iconame = inmd[current_CS->in_method].icon;
+  if (cur_inmd)
+    iconame = cur_inmd->icon;
   char fname[512];
   if (iconame)
     get_icon_path(iconame, fname);
