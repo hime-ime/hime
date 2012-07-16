36eb974e36961271f405a55782ac2eeb145e4575
Tetralet <tetralet@gmail.com>
2012-07-16 10:30:33 +0800

    Clear a runtime warning when running under onthespot mode.

--- src/win-gtab.c
+++ src/win-gtab.c
@@ -642,6 +642,9 @@ void create_win_gtab_gui_simple()
 
 void show_input_method_name_on_gtab()
 {
+  // label_gtab = NULL under onthespot mode.
+  if (label_gtab==NULL) return;
+
   if (current_CS && (! hime_status_tray) && gtab_hide_row2 && gtab_disp_im_name &&
       (current_CS->im_state == HIME_STATE_CHINESE) && (current_CS->b_half_full_char == 0) &&
       (current_CS->tsin_pho_mode))
