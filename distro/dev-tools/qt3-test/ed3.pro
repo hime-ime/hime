772ffdbcb68c1f0ce2df9e5143ac6f2c5354747b
xatier <xatierlike@gmail.com>
2020-05-10 01:08:32 -0700

    Drop QT3 support

--- distro/dev-tools/qt3-test/ed3.pro
+++
@@ -1,5 +0,0 @@
-TEMPLATE = app
-DEPENDPATH += .
-INCLUDEPATH += .
-HEADERS += ed3.h
-SOURCES += ed3a.cpp ed3.cpp
--- distro/dev-tools/qt3-test/ed3a.cpp
+++
@@ -1,6 +0,0 @@
-#include "ed3.h"
-
-LineEdits::LineEdits(QWidget *parent, const char *name)
-{
-  lined1 = new QLineEdit(this);
-}
