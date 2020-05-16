8d6ab843606f8cef8a9eaa40ecdf8a6a94622041
xatier <xatierlike@gmail.com>
2020-05-16 09:59:47 -0700

    Add qt5-base to Archlinux CI dependency

--- distro/archlinux/ci/entrypoint.sh
+++ distro/archlinux/ci/entrypoint.sh
@@ -21,7 +21,8 @@ pacman -Syuu --noconfirm --needed \
     anthy \
     libchewing \
     libappindicator-gtk2 \
-    libappindicator-gtk3
+    libappindicator-gtk3 \
+    qt5-base
 
 echo 'en_US.UTF-8 UTF-8' >/etc/locale.gen && locale-gen
 
