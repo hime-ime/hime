7ffb28e9ddf135473250dccc9139b53e10c91b9f
xatier <xatierlike@gmail.com>
2020-05-17 04:53:24 -0700

    Drop Qt4 code

--- distro/dev-tools/qt4-test/ed4.cpp
+++
@@ -1,9 +0,0 @@
-#include "ed4.h"
-
-int main()
-{
-  QApplication a(0, 0);
-  linedit *l=new linedit;
-  l->gui_init();
-  return a.exec();
-}
--- distro/dev-tools/qt4-test/ed4.pro
+++
@@ -1,5 +0,0 @@
-TEMPLATE = app
-DEPENDPATH += .
-INCLUDEPATH += .
-HEADERS += ed4.h
-SOURCES += ed4a.cpp ed4.cpp
