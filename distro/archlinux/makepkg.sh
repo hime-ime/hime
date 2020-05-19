e3aa575a047c62eec4a17ef9e5eb5d4934b21939
xatier <xatierlike@gmail.com>
2020-05-18 17:22:28 -0700

    Add makepkg.sh to build Archlinux package

---
+++ distro/archlinux/makepkg.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+topleveldir="$(git rev-parse --show-toplevel)"
+archdir="$topleveldir/distro/archlinux"
+
+pushd "$archdir" || exit 1
+set -x
+
+# clean up
+rm -rf hime/ pkg/ src/
+
+makepkg -sf
+
+ls -ltr ./*.pkg.tar.xz
+
+set +x
+popd || exit 1
