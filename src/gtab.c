e929f9ebb1ae0cbd0e2a841112e12e39e2fe61de
marktwtn <marktwtn@gmail.com>
2022-03-24 00:38:37 +0800

    refactor: Move feedkey_gtab(_release) into the same file
    The feedkey_* and feedkey_*_release functions should be located in the
    same file as they are related to each other.

--- src/gtab.c
+++ src/gtab.c
@@ -1743,3 +1743,38 @@ Disp_opt:
 
     return 1;
 }
+
+void tsin_toggle_eng_ch ();
+
+int feedkey_gtab_release (KeySym xkey, int kbstate) {
+    switch (xkey) {
+    case XK_Control_L:
+    case XK_Control_R:
+        if (key_press_ctrl && tss.pre_selN) {
+            if (!test_mode) {
+                tss.ctrl_pre_sel = TRUE;
+            }
+            key_press_ctrl = FALSE;
+            return 1;
+        } else
+            return 0;
+#if 1
+    case XK_Shift_L:
+    case XK_Shift_R:
+        // dbg("release xkey %x\n", xkey);
+        if (((tsin_chinese_english_toggle_key == TSIN_CHINESE_ENGLISH_TOGGLE_KEY_Shift) ||
+             (tsin_chinese_english_toggle_key == TSIN_CHINESE_ENGLISH_TOGGLE_KEY_ShiftL && xkey == XK_Shift_L) ||
+             (tsin_chinese_english_toggle_key == TSIN_CHINESE_ENGLISH_TOGGLE_KEY_ShiftR && xkey == XK_Shift_R)) &&
+            key_press_alt) {
+            if (!test_mode) {
+                tsin_toggle_eng_ch ();
+            }
+            key_press_alt = FALSE;
+            return 1;
+        } else
+            return 0;
+#endif
+    default:
+        return 0;
+    }
+}
